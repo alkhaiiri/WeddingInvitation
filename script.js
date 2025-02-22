59e9864f0215850b545a319423d7e8e32a518ab5
M Alkhairi Mashuri <muhammad.alkhairi@gmail.com>
2025-02-22 23:11:29 +0800

    update script

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-// var tl = gsap.timeline()
-
-// tl.from("#loader h2",{
-//   x:50,
-//   opacity:0,
-//   duration:3,
-//   stagger:0.5
-// })
-// tl.to("#loader h2",{
-//   x:-20,
-//   opacity:0,
-//   duration:2,
-//   stagger:-0.5
-// })
-// tl.to("#loader",{
-//   opacity:0,
-//   display:"none",
-// })
 var tl = gsap.timeline();
 
-tl.to("#loader h2:nth-child(1)", { opacity: 1, duration: 1 })
-  .to("#loader h2:nth-child(1)", { opacity: 0, duration: 1, delay: 1 }) // Fade out first text
-  .to("#loader h2:nth-child(2)", { opacity: 1, duration: 1 })
-  .to("#loader h2:nth-child(2)", { opacity: 0, duration: 1, delay: 1 }) // Fade out second text
-  // .to("#loader h2:nth-child(3)", { opacity: 1, duration: 1 })
-  // .to("#loader h2:nth-child(3)", { opacity: 0, duration: 1, delay: 1 }) // Fade out third text
-  .to("#loader", { opacity: 0, display: "none", duration: 1 }); // Hide loader
+tl.fromTo("#loader h2:nth-child(1)", { opacity: 0, x: "100%" }, { opacity: 1, x: "0%", duration: 1 })
+  .to("#loader h2:nth-child(1)", { opacity: 0, x: "-100%", duration: 1, delay: 1 })
+  .fromTo("#loader h2:nth-child(2)", { opacity: 0, x: "100%" }, { opacity: 1, x: "0%", duration: 1 })
+  .to("#loader h2:nth-child(2)", { opacity: 0, x: "-100%", duration: 1, delay: 1 })
+  .to("#loader", { opacity: 0, display: "none", duration: 1 });
