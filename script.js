44a803a36094432f39ba0b519c57514578eef52d
M Alkhairi Mashuri <muhammad.alkhairi@gmail.com>
2025-02-23 23:50:34 +0800

    update

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 var tl = gsap.timeline();
 
-// tl.fromTo("#loader h2:nth-child(1)", { opacity: 0, x: "100%" }, { opacity: 1, x: "0%", duration: 1 })
-//   .to("#loader h2:nth-child(1)", { opacity: 0, x: "-100%", duration: 1, delay: 1 })
-//   .fromTo("#loader h2:nth-child(2)", { opacity: 0, x: "100%" }, { opacity: 1, x: "0%", duration: 1 })
-//   .to("#loader h2:nth-child(2)", { opacity: 0, x: "-100%", duration: 1, delay: 1 })
-//   .to("#loader", { opacity: 0, display: "none", duration: 1 });
-
 tl.to(".lording", { opacity: 1, duration: 0.7 })
   .to(".lording", {
     opacity: 0,
