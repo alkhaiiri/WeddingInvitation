7cd6304580e3ff018fef700fa81ce5b496e36e6b
M Alkhairi Mashuri <muhammad.alkhairi@gmail.com>
2025-06-03 23:45:54 +0800

    update content

--- assets/script.js
+++ assets/script.js
@@ -51,17 +51,8 @@ setTimeout(function () {
 var hbdmp = document.getElementById("hbdmp");
 var icon = document.getElementById("icon");
 
-// icon.onclick = function () {
-//   if (hbdmp.paused) {
-//     hbdmp.play();
-//     icon.src = "images/pause.png";
-//   } else {
-//     hbdmp.pause();
-//     icon.src = "images/play.png";
-//   }
-// };
-
 document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
   const card = document.querySelector(".card");
   const s1 = document.querySelector(".s-1");
   const s2 = document.querySelector(".s-2");
@@ -69,28 +60,204 @@ document.addEventListener("DOMContentLoaded", () => {
   function expandCard() {
     hbdmp.muted = false;
     hbdmp.play();
-    // Hide s-1 and s-2 smoothly
+
     s1.style.opacity = "0";
     s2.style.opacity = "0";
 
     setTimeout(() => {
-      s1.style.display = "none"; // Completely hide after fading
+      s1.style.display = "none";
       s2.style.display = "none";
     }, 1500);
 
-    // Smooth expansion
     card.classList.add("expanded");
 
-    // Optionally, remove fixed width/height after expansion
     setTimeout(() => {
-      card.style.width = "100vw";
-      card.style.height = "100vh";
-    }, 1000); // Adjust the timing based on the animation speed
+      card.style.width = "100%";
+      card.style.height = "100%";
+      AOS.init({
+        container: document.querySelector('.container-invitation'),
+        duration: 1000,
+        easing: 'ease-in-out',
+        // once: true,
+        offset: 120,
+        callback: function(el) {
+          console.log('AOS triggered:', el);
+        }
+      });
+    }, 1000);
   }
 
-  // card.addEventListener("click", expandCard);
   card.addEventListener("click", function () {
-    card.classList.toggle("active");
-    expandCard();
+    if (!card.classList.contains("active")) {
+      card.classList.add("active");
+      expandCard();
+      confirm_open_invitation();
+    }
   });
 });
+
+function confirm_open_invitation() {
+  var container = document.querySelector(".container-invitation");
+
+  container.classList.add(
+    "animate__animated",
+    "animate__fadeIn",
+    "animate__slow"
+  );
+
+  setTimeout(function () {
+    container.classList.remove("hide");
+
+    let observer = new IntersectionObserver(
+      (entries, observer) => {
+        console.log('AOS refresh');
+        AOS.refresh();
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log(`Intersecting: ${entry.target.id}`);
+            $(".navItem").removeClass("active");
+            if (entry.target.id === "cover") {
+              $(".navItem").removeClass("active");
+            } else if (entry.target.id === "bridegroom") {
+              $(".bridegroomNavItem").addClass("active");
+            } else if (entry.target.id === "event") {
+              $(".eventNavItem").addClass("active");
+            } else if (entry.target.id === "story") {
+              $(".storyNavItem").addClass("active");
+            } else if (entry.target.id === "galery") {
+              $(".galeryNavItem").addClass("active");
+            } else if (entry.target.id === "rsvp") {
+              $(".rsvpNavItem").addClass("active");
+            }
+            if (entry.target.id === "footer") {
+              $(".nav").addClass("hide");
+            } else {
+              $(".nav").removeClass("hide");
+            }
+          }
+        });
+      },
+      { rootMargin: "0px", threshold: 0.3 }
+    );
+    document.querySelectorAll("section").forEach((section) => {
+      observer.observe(section);
+    });
+  }, 1000);
+
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+    disableScrolling: true,
+  });
+}
+
+window.onload = function (){
+  fallingFlower();
+}
+
+function fallingFlower() {
+  TweenLite.set("#container-invitation", { perspective: 600 });
+
+  var total = 20;
+  var container = document.getElementById("container-invitation"),
+      w = window.innerWidth,
+      h = window.innerHeight;
+
+  for (i = 0; i < total; i++) {
+      var Div = document.createElement("div");
+      var Div2 = document.createElement("div");
+      var Div3 = document.createElement("div");
+      TweenLite.set(Div, {
+          attr: { class: "dot" },
+          x: R(0, w),
+          y: 0,
+          z: 0,
+      });
+      TweenLite.set(Div2, {
+          attr: { class: "dot2" },
+          x: R(0, w),
+          y: 0,
+          z: 0,
+      });
+      TweenLite.set(Div3, {
+          attr: { class: "dot3" },
+          x: R(0, w),
+          y: 0,
+          z: 0,
+      });
+      container.appendChild(Div);
+      container.appendChild(Div2);
+      container.appendChild(Div3);
+      animm(Div);
+      animm2(Div2);
+      animm3(Div3);
+  }
+
+  function animm(elm) {
+      TweenMax.to(elm, R(6, 15), {
+          y: h + 100,
+          ease: Linear.easeNone,
+          repeat: -1,
+          delay: -15,
+      });
+      TweenMax.to(elm, R(4, 8), {
+          x: "+=100",
+          rotationZ: R(0, 180),
+          repeat: -1,
+          yoyo: true,
+          ease: Sine.easeInOut,
+      });
+      TweenMax.to(elm, R(2, 8), {
+          repeat: -1,
+          yoyo: true,
+          ease: Sine.easeInOut,
+          delay: -5,
+      });
+  }
+  function animm2(elm) {
+      TweenMax.to(elm, R(6, 15), {
+          y: h + 100,
+          ease: Linear.easeNone,
+          repeat: -1,
+          delay: -25,
+      });
+      TweenMax.to(elm, R(4, 8), {
+          x: "+=100",
+          rotationZ: R(0, 180),
+          repeat: -1,
+          yoyo: true,
+          ease: Sine.easeInOut,
+      });
+      TweenMax.to(elm, R(2, 8), {
+          repeat: -1,
+          yoyo: true,
+          ease: Sine.easeInOut,
+          delay: -5,
+      });
+  }
+  function animm3(elm) {
+      TweenMax.to(elm, R(6, 15), {
+          y: h + 100,
+          ease: Linear.easeNone,
+          repeat: -1,
+          delay: -5,
+      });
+      TweenMax.to(elm, R(4, 8), {
+          x: "+=100",
+          rotationZ: R(0, 180),
+          repeat: -1,
+          yoyo: true,
+          ease: Sine.easeInOut,
+      });
+      TweenMax.to(elm, R(2, 8), {
+          repeat: -1,
+          yoyo: true,
+          ease: Sine.easeInOut,
+          delay: -5,
+      });
+  }
+
+  function R(min, max) {
+      return min + Math.random() * (max - min);
+  }
+}
