14ba7b17b100e43a2a16869bbd772c697875460a
M Alkhairi Mashuri <muhammad.alkhairi@gmail.com>
2025-06-06 22:01:38 +0800

    update welcome text

--- assets/script.js
+++ assets/script.js
@@ -12,35 +12,35 @@ tl.to(".lording", { opacity: 1, duration: 0.7 })
   .fromTo(
     "#loader h2:nth-child(1)",
     { opacity: 0, x: "100%" },
-    { opacity: 1, x: "0%", duration: 2 }
+    { opacity: 1, x: "0%", duration: 1 }
   )
   .to("#loader h2:nth-child(1)", {
     opacity: 0,
     x: "-100%",
     duration: 1,
-    delay: 0.5,
+    delay: 1,
   })
   .fromTo(
     "#loader h2:nth-child(2)",
     { opacity: 0, x: "100%" },
-    { opacity: 1, x: "0%", duration: 1.5 }
+    { opacity: 1, x: "0%", duration: 1 }
   )
   .to("#loader h2:nth-child(2)", {
     opacity: 0,
     x: "-100%",
     duration: 1,
-    delay: 0.5,
+    delay: 1,
   })
   .fromTo(
     "#loader h2:nth-child(3)",
     { opacity: 0, x: "100%" },
-    { opacity: 1, x: "0%", duration: 1.5 }
+    { opacity: 1, x: "0%", duration: 1 }
   )
   .to("#loader h2:nth-child(3)", {
     opacity: 0,
     x: "-100%",
     duration: 1,
-    delay: 0.5,
+    delay: 1,
   })
   .to("#loader", { opacity: 0, display: "none", duration: 1 });
 
